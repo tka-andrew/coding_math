7e8b630c6cf546945dce50acfc1207b89185f3cc
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-10-23 13:41:25 +0800

    ep06_ep07_vector

---
+++ js/vector.js
@@ -0,0 +1,87 @@
+var vector = {
+    _x: 1,
+    _y: 0,
+
+    create: function (x, y) {
+        let obj = Object.create(this);
+        obj.setX(x);
+        obj.setY(y);
+        return obj;
+    },
+
+    setX: function (val) {
+        this._x = val;
+    },
+
+    getX: function () {
+        return this._x;
+    },
+
+    setY: function (val) {
+        this._y = val;
+    },
+
+    getY: function () {
+        return this._y;
+    },
+
+    setLength: function () {
+        let angle = this.getAngle();
+        this._x = length * Math.cos(angle);
+        this._y = length * Math.sin(angle);
+    },
+
+    getLength: function () {
+        return Math.sqrt(this._x * this._x + this._y * this._y);
+    },
+
+    setAngle: function (val) {
+        let length = this.getLength();
+        this._x = length * Math.cos(val);
+        this._y = length * Math.sin(val);
+    },
+
+    getAngle: function (val) {
+        return Math.atan2(this._y, this._x);
+    },
+
+    add: function (v2) {
+        return vector.create(this._x + v2.getX(), this._y + v2.getY());
+    },
+
+    subtract: function (v2) {
+        return vector.create(this._x - v2.getX(), this._y - v2.getY());
+    },
+
+    multiply: function (val) {
+        return vector.create(this._x * val, this._y * val);
+    },
+
+    divide: function (val) {
+        return vector.create(this._x / val, this._y / val);
+    },
+
+    // in place update
+    addTo: function (v2) {
+        this._x += v2.getX();
+        this._y += v2.getY();
+    },
+
+    // in place update
+    subtractFrom: function (v2) {
+        this._x -= v2.getX();
+        this._y -= v2.getY();
+    },
+
+    // in place update
+    multiplyBy: function (val) {
+        this._x *= val;
+        this._y *= val;
+    },
+    
+    // in place update
+    divideBy: function (val) {
+        this._x /= val;
+        this._y /= val;
+    },
+};
---
+++ test/test_vector.js
@@ -0,0 +1,7 @@
+let v1 = vector.create(10, 5);
+let v2 = vector.create(10, 5);
+let v3 = v1.add(v2);
+console.log(v3)
+console.log(v3.multiply(2))
+v3.multiplyBy(2)
+console.log(v3)
